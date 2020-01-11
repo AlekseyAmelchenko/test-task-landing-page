8613f2373533a1d9a6be80830b6f5f7e7f99da32
Aleksey <aleksey.amelchenko@gmail.com>
2020-01-11 16:23:02 +0300

    Add colors

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export default {
+  space: [2, 4, 8, 16, 32, 64, 128],
+  radii: [0, "2px", "4px", "8px"],
+  fontSizes: [12, 14, 16, 20, 24, 28, 32, 40, 48, 64],
+  colors: {
+    red: "#F00",
+    grey: "#333",
+    stroke: "#BDBDBD",
+    option: "#DADADA",
+    green: "#009C37",
+    darkGreen: "#076e31",
+    white: "#FFF",
+    gray: "#F2F2F2"
+  }
+};
